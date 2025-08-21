c8ee96ac88d6e20e3b19956466138b1c2fc3c462
etikivishwas <etikivishwas47@gmail.com>
2025-08-21 17:48:04 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { AuthProvider } from './contexts/AuthContext';
 import Login from './components/Login/Login.jsx';
+import Navbar from './components/Navbar/Navbar.jsx';
 import { GOOGLE_CLIENT_ID } from './config/googleOAuth';
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
       <AuthProvider>
         <div className="App">
           <Login />
+          {/* <Navbar /> */}
         </div>
       </AuthProvider>
     </GoogleOAuthProvider>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import styles from './styles.module.css'
+import { FaBuilding, FaBell } from 'react-icons/fa';
+import Image from './passport.jpg'
 
 function Navbar() {
   return (
-    <div>
-      
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <FaBuilding className={styles.icon} />
+        <h2 className={styles.title}>CorporateIntern</h2>
+      </div>
+      <div className={styles.links}>
+        <a href="#" className={styles.link}>Dashboard</a>
+        <a href="#" className={styles.link}>Projects</a>
+        <a href="#" className={styles.link}>Tickets</a>
+        <a href="#" className={styles.link}>Documents</a>
+        <a href="#" className={styles.link}>Chats</a>
+        <a href="#" className={styles.link}>Mentorship</a>
+        <a href="#" className={styles.link}>Analytics</a>
+      </div>
+      <div className={styles.user}>
+        <div className={styles.notification}>
+          <span className={styles.badge}>5</span>
+          <FaBell />
+        </div>
+        <img
+          src={Image}
+          alt="User"
+          className={styles.avatar}
+        />
+        <span className={styles.username}>Alex Kumar</span>
+      </div>
     </div>
   )
 }
